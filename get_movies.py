04933c3fbd6282c819a632f6606d3e9b543544e0
Tianhao <120090472@link.cuhk.edu.cn>
2022-10-17 17:01:33 +0800

    added arg parser

--- get_movies.py
+++ get_movies.py
@@ -2,12 +2,11 @@ import re
 import time
 import xlwt
 import requests
-from lxml import etree
+import configargparse
 from bs4 import BeautifulSoup as bs
 
 # 'User-Agent'：模拟浏览器访问
 # 'Cookie'：模拟登录，绕过滑动拼图验证
-baseURL = 'https://maoyan.com/board/4'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42',
     'Cookie': '__mta=107315005.1665939224502.1665939224502.1665974803574.2; uuid_n_v=v1; uuid=173859204D7311ED85D65D754909B702076AF11DCDA544D8A900642BFA0187D6; _csrf=5432c7e2dc15d94264501c4618c9be1e08e65e61718496ecb14fa396186921fc; _lxsdk_cuid=183e1b733d2c8-0ea3069247b23a-4f6e117b-13c680-183e1b733d2c8; _lxsdk=173859204D7311ED85D65D754909B702076AF11DCDA544D8A900642BFA0187D6; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1665939224; __mta=121347895.1665974321686.1665974321686.1665974321686.1; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1665974803; _lxsdk_s=183e3cec045-f02-785-f64%7C%7C4'
@@ -22,7 +21,7 @@ def visit_page(url):
     return bs(page.content, 'lxml')
 
 # 从网页爬取数据
-def scraping():
+def scraping(baseURL, save_html_folder):
     # 获得全部页面信息存入列表中
     board_content_list = []
     for offset in range(10):
@@ -30,18 +29,19 @@ def scraping():
         board_page = visit_page(board_url)
         board_content_list.append(board_page)
         time.sleep(0.5)
-        with open(f'pages/offset_{offset}.html', 'w+', encoding='utf-8') as f:
-            f.write(str(board_page.prettify()))
+        if save_html_folder:
+            with open(f'{save_html_folder}/offset_{offset}.html', 'w+', encoding='utf-8') as f:
+                f.write(str(board_page.prettify()))
     return board_content_list
 
-
+# 下载图片
 def download_image(url, name):
     img_format = re.findall('.jpg|.png|.webp', url)[0]
     with open('images/' + name + img_format, 'wb') as f:
         f.write(requests.get(url).content)
 
-
-def process_metadata(metadta):
+# 处理每个电影的信息
+def process_metadata(metadta, download_image=True):
     movie_attrs = {
                 'index':{'tag': 'i', 'class_':'board-index'}, 
                 'name':{'tag': 'p', 'class_':'name'},
@@ -56,8 +56,8 @@ def process_metadata(metadta):
         _info = metadta.find(attr_dict['tag'], class_=attr_dict['class_'])
         if not(attr == 'image'):
             _info = _info.text.strip()
-        # else:
-        #     download_image(url= _info['data-src'], title= _info['alt'])
+        elif download_image:
+            download_image(url= _info['data-src'], title= _info['alt'])
         if attr in ('star', 'releasetime'):
             _info = re.split(':|：', _info)[1]
         ret_info[attr] = _info
@@ -65,8 +65,8 @@ def process_metadata(metadta):
     return ret_info
 
 
-# 处理数据
-def batch_retrive_metadata(board_content_list):
+# 从每个网页中找到电影信息
+def batch_retrive_metadata(board_content_list, download_image=True):
     # 从页面上爬取每个电影的信息
     # index = [i for i in range(1, 101)]
     raw_movie_metadata = []
@@ -75,12 +75,12 @@ def batch_retrive_metadata(board_content_list):
         # for key in raw_info.keys():
         raw_movie_metadata += board_content.find_all('dd')
     
-    processed_movie_metadata = [process_metadata(_) for _ in raw_movie_metadata]
+    processed_movie_metadata = [process_metadata(_, download_image=True) for _ in raw_movie_metadata]
     return processed_movie_metadata
 
 
 # 将爬取到的数据存入Excel文件中
-def build_excel_file(data):
+def build_excel_file(data, file_name):
     # 创建一个Excel文件
     f = xlwt.Workbook(encoding='UTF-8')
     # 创建一个sheet
@@ -93,19 +93,29 @@ def build_excel_file(data):
     for idx, entry in enumerate(data):
         for k_idx, key in enumerate(entry.keys()):
             sheet1.write(idx, k_idx, entry[key])
-    f.save('猫眼电影榜单Top100.xls')
+    f.save(file_name)
+
+def config_parser():
+    parser = configargparse.ArgumentParser()
+    parser.add_argument('--baseURL', '-b', type=str, default='https://maoyan.com/board/4?offset=', help='base url')
+    parser.add_argument('--download_image', '-d',type=bool, default=True, help='download image')
+    parser.add_argument('--save_html_folder', '-s', type=str, default='./pages', help='If specified, save html to this folder.')
+    parser.add_argument('--excel_file_name', '-n', type=str, default='猫眼电影榜单Top100.xls', help='excel file name')
+    return parser.parse_args()
+
 
 def main():
     print("################程序运行开始################")
+    args = config_parser()
     print("<---------------开始爬取数据--------------->")
-    board_html = scraping()
+    board_html = scraping(args.baseURL, args.save_html_folder)
     print("<---------------数据爬取完成--------------->")
     print("<---------------开始处理数据--------------->")
-    data = batch_retrive_metadata(board_html)
+    data = batch_retrive_metadata(board_html, args.download_image)
     print("<---------------数据处理完成--------------->")
     print("<---------------开始存储数据--------------->")
     print("将爬取数据存入Excel文件：")
-    build_excel_file(data)
+    build_excel_file(data, args.excel_file_name)
     print("<---------------数据存储完成--------------->")
     print("################程序运行结束################")
 
